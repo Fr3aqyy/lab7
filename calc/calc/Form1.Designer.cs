625dce4c86a3562eed72a7ccbc63edb08ae15ba9
Ivan <lii190601@gmail.com>
2024-05-16 14:59:20 +0300

    Цвет черный

--- calc/calc/Form1.Designer.cs
+++ calc/calc/Form1.Designer.cs
@@ -235,7 +235,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
+            BackColor = SystemColors.ActiveCaptionText;
             ClientSize = new Size(309, 445);
             Controls.Add(button16);
             Controls.Add(button15);
